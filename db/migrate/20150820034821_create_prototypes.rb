01f26461cf3a5ef9843bc5f0c06fa900c195fcd7
kohei_uetan <r0cknet@r0ckNethikar1-no-MacBook-Pro.local>
2015-08-20 12:58:50 +0900

    prototypeに関するモデル、コントローラー、ビューファイル,テーブルを作りました。

---
+++ db/migrate/20150820034821_create_prototypes.rb
@@ -0,0 +1,13 @@
+class CreatePrototypes < ActiveRecord::Migration
+  def change
+    create_table :prototypes do |t|
+      t.text :title
+      t.text :introduction
+      t.text :genre
+      t.integer :user_id
+      t.text :github
+      t.text :url
+      t.timestamps
+    end
+  end
+end
