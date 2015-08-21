e321c153e3b08323886a3a3399d4154a4825d806
kohei_uetan <r0cknet@r0ckNethikar1-no-MacBook-Pro.local>
2015-08-21 20:41:15 +0900

    変数名を変更し、coding styleを修正しました。

--- app/controllers/prototypes/latest_controller.rb
+++ app/controllers/prototypes/latest_controller.rb
@@ -2,6 +2,6 @@ class Prototypes::LatestController < ApplicationController
 
   def index
     # @latests=Prototype.order('created_at DESC').limit(10)
-    @latests=Prototype.all
+    @prototypes = Prototype.all
   end
 end
--- app/controllers/prototypes/ranking_controller.rb
+++ app/controllers/prototypes/ranking_controller.rb
@@ -1,7 +1,7 @@
 class Prototypes::RankingController < ApplicationController
 
   def index
-    @rankings=Prototype.all
+    @rankings = Prototype.all
     # @rankings=Prototype.all.limit()
   end
 end
