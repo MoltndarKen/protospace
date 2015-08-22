618fe1e69a5bde1a19bc381547853afd230af741
kohei_uetan <r0cknet@r0ckNethikar1-no-MacBook-Pro.local>
2015-08-22 11:32:27 +0900

    # what * delete comment_out in latest_controller,ranking_controller

--- app/controllers/prototypes/latest_controller.rb
+++ app/controllers/prototypes/latest_controller.rb
@@ -1,7 +1,6 @@
 class Prototypes::LatestController < ApplicationController
 
   def index
-    # @latests=Prototype.order('created_at DESC').limit(10)
     @prototypes = Prototype.all
   end
 end
--- app/controllers/prototypes/ranking_controller.rb
+++ app/controllers/prototypes/ranking_controller.rb
@@ -2,6 +2,5 @@ class Prototypes::RankingController < ApplicationController
 
   def index
     @rankings = Prototype.all
-    # @rankings=Prototype.all.limit()
   end
 end
