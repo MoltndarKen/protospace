17fd4429acf727c5e3c0ccd561bc29783b213d49
kohei_uetan <r0cknet@r0ckNethikar1-no-MacBook-Pro.local>
2015-08-23 18:24:36 +0900

    # WHAT add column name,work,member,github to users table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150823091249) do
+ActiveRecord::Schema.define(version: 20150823092213) do
 
   create_table "prototypes", force: true do |t|
     t.string   "title"
@@ -39,6 +39,10 @@ ActiveRecord::Schema.define(version: 20150823091249) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
+    t.string   "name"
+    t.string   "work"
+    t.string   "member"
+    t.text     "github"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
