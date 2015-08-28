864db6e60310da30ac85f89e9fe7183b1dbe9e10
kohei_uetan <r0cknet@r0ckNethikar1-no-MacBook-Pro.local>
2015-08-28 18:30:04 +0900

    # WHAT edit login view file by using form_for method # why to Implement the login function

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,5 +2,15 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   before_action :authenticate_user!, except: [:index]
+  before_action :configure_permitted_parameters ,if: :devise_controller?
   protect_from_forgery with: :exception
 end
+
+def after_sign_out_path_for(resource)
+  user_session_path
+end
+
+def configure_permitted_parameters
+  devise_parameter_sanitizer.for(:sign_up).push(:name,:work,:member,:profile)
+end
+
