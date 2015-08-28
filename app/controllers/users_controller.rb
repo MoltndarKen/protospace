84203a2593866e0e015bbbd19d9eca5597719099
kohei_uetan <r0cknet@r0ckNethikar1-no-MacBook-Pro.local>
2015-08-28 12:06:49 +0900

    # WHAT routing users by using 'devise_for' method # WHY couldn't use devise helper method(user_signed_in?) because of not using 'devise_for' method

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,9 +5,16 @@ class UsersController < ApplicationController
   def new
   end
 
+  def create
+    redirect_to prototypes_latest_index_path
+  end
+
   def edit
   end
 
   def sign_in
+  end
+  private
+  def create_params
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  devise_for :users
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
@@ -57,7 +58,6 @@ Rails.application.routes.draw do
     resources :ranking ,only: [:index]
     resources :latest ,only: [:index]
   end
-  get 'users/sign_in' => 'users#sign_in'
-  resources :users, only: [:new, :create]
-  get 'users/:id' => 'users#show'
+
 end
+
