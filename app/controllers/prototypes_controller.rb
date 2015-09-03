c2c97878514ff60ee92bfcfc119e23cbefee4517
kohei_uetan <r0cknet@r0ckNethikar1-no-MacBook-Pro.local>
2015-09-03 23:29:39 +0900

    # what&why edit prototype controller to post 'tag'

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -1,3 +1,13 @@
 class PrototypesController < ApplicationController
+  def new
+    @prototype=Prototype.new
+  end
+  def create
+    Prototype.create(create_params)
+  end
+  private
+  def create_params
+    params.require(:prototype).permit(:title,:introduction,:genre,:url,:tag_list)
+  end
 end
 
