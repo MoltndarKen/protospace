e20404fc8914a55abeb06ade24d129bb00eaac41
kohei_uetan <r0cknet@r0ckNethikar1-no-MacBook-Pro.local>
2015-09-04 19:03:45 +0900

    # what&why generate catch copy, concept columns to Prototypes tables

--- config/application.rb
+++ config/application.rb
@@ -13,6 +13,7 @@ module Protospace
         g.helper false
         g.assets false
     end
+    
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150903133453) do
+ActiveRecord::Schema.define(version: 20150904100220) do
 
   create_table "prototypes", force: true do |t|
     t.string   "title"
@@ -22,6 +22,8 @@ ActiveRecord::Schema.define(version: 20150903133453) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.integer  "user_id"
+    t.text     "catch_copy"
+    t.text     "concept"
   end
 
   add_index "prototypes", ["user_id"], name: "index_prototypes_on_user_id", using: :btree
