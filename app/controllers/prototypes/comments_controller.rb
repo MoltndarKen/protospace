45d8a5a539b862a6e9f2d11eed9fea23f67ee4ce
kohei_uetan <r0cknet@r0ckNethikar1-no-MacBook-Pro.local>
2015-09-06 18:12:02 +0900

    # what&why edit action to save comments and redirect_to show page

--- app/controllers/prototypes/comments_controller.rb
+++ app/controllers/prototypes/comments_controller.rb
@@ -1,6 +1,8 @@
 class Prototypes::CommentsController < ApplicationController
 
   def create
+    Comment.build(create_params).save
+    redirect_to prototype_path
   end
   private
   def create_params
