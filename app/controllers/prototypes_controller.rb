4669f1d3f07ce9b04acd3a904c63b9dd392aac0f
kohei_uetan <r0cknet@r0ckNethikar1-no-MacBook-Pro.local>
2015-09-06 16:18:56 +0900

    # WHAT&WHY can upload multiple images to prototype

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -1,14 +1,16 @@
 class PrototypesController < ApplicationController
   def new
     @prototype=Prototype.new
+    3.times { @prototype.captured_images.build }
   end
   def create
+    binding.pry
     Prototype.create(create_params)
     redirect_to action: "new"
   end
   private
   def create_params
-    params.require(:prototype).permit(:title, :catch_copy, :concept, :genre, :url,).merge(tag_list: params[:list][:tag])
+    params.require(:prototype).permit(:title, :catch_copy, :concept, :genre, :url,captured_images_attributes: [:image]).merge(tag_list: params[:list][:tag])
   end
 end
 
