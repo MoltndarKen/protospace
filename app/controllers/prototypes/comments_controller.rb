9498700ab0c75ed5999eaef52ece0e9082fa47fd
kohei_uetan <r0cknet@r0ckNethikar1-no-MacBook-Pro.local>
2015-09-07 00:24:05 +0900

    # what&why implemets comment systems using ajax

--- app/controllers/prototypes/comments_controller.rb
+++ app/controllers/prototypes/comments_controller.rb
@@ -3,9 +3,9 @@ class Prototypes::CommentsController < ApplicationController
   def new
   end
   def create
-    prototype = Prototype.find(params[:id])
-    prototype.comments.build(text: create_params[:text], user_id: current_user.id).save
-    redirect_to prototype_path
+    @prototype = Prototype.find(params[:id])
+    @prototype.comments.build(text: create_params[:text], user_id: current_user.id).save
+    # redirect_to prototype_path
   end
   private
   def create_params
