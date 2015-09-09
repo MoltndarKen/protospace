dda67ee41d58d8540311e65f0d737d9f3c48a889
kohei_uetan <r0cknet@r0ckNethikar1-no-MacBook-Pro.local>
2015-09-10 00:48:19 +0900

    # what&why send property by sending hidden_field

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -1,19 +1,22 @@
 class PrototypesController < ApplicationController
   def new
     @prototype=Prototype.new
-    3.times{@prototype.captured_images.build}
+    @prototype.captured_images.build
   end
   def create
     @prototype = current_user.prototypes.build(create_params)
-    @prototype.captured_images.each.with_index(1) do |image,i|
-      i == 1 ? image.property = "main" : image.property = "sub"
+    @prototype.captured_images.each  do |image|
+      # i == 1 ? image.property = "main" : image.property = "sub"
     end
     @prototype.save
     redirect_to root_path
   end
   private
   def create_params
-    params.require(:prototype).permit(:title, :catch_copy, :concept, :genre, :url,captured_images_attributes: [:image]).merge(tag_list: params[:list][:tag])
+    params.require(:prototype).permit(:title, :catch_copy, :concept, :genre, :url,captured_images_attributes: [:image,:property]).merge(tag_list: params[:list][:tag])
+  end
+  def propetry_params
+    params.require(:prototype).permit(captured_images_attributes: [:property])
   end
 end
 
