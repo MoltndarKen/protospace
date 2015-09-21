529579bf973fe65b02604307efb8e6edca4e1277
kohei_uetan <r0cknet@r0ckNethikar1-no-MacBook-Pro.local>
2015-09-22 00:04:50 +0900

    # what&why refucturing tag links between index and show

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -1,4 +1,10 @@
 class TagsController < ApplicationController
   def index
+    @tags = ActsAsTaggableOn::Tag.most_used(100)
+  end
+  def show
+    @tag = ActsAsTaggableOn::Tag.find_by(params[:id])
+    proto_tags = ActsAsTaggableOn::Tagging.where(tag_id: params[:id])
+    @prototypes = proto_tags.map{|proto_tag| Prototype.find(proto_tag.taggable_id)}
   end
 end
